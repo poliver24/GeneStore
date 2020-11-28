a057524cb42fa97e199ec1a4b6a36a03cb7344a4
Patrick Oliver <poliver24@hotmail.com>
2020-11-28 23:16:59 +0000

    Gene model created; Basic test for gene model made

--- immunocore_project/immunocore_app/models.py
+++ immunocore_project/immunocore_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Gene(models.Model):
+    """
+    Gene Model
+    Defines the attributes of a Gene
+    """
+
+    name = models.CharField(max_length=255)
+    sequence = models.CharField(max_length=10000)
+
+
+    # Method added for basic model testing
+    def get_sequence(self):
+        return self.name + ' has sequence ' + self.sequence + '.'
+
+    def __repr__(self):
+        return self.name + ' has been added.'
--- immunocore_project/immunocore_project/settings.py
+++ immunocore_project/immunocore_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$qr)&vyeusvb^w@x9ok@rg0#zqe&iuw**-4o%csbuvv^eh#2@y'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'immunocore_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'immunocore_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

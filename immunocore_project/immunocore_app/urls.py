27f718f99c034b88596316d4857d938a272ab5bd
Patrick Oliver <poliver24@hotmail.com>
2020-11-29 11:56:53 +0000

    Updated Serializer to include ID; Created Superuser; Updated Models to return __str__

--- immunocore_project/immunocore_app/urls.py
+++ immunocore_project/immunocore_app/urls.py
@@ -4,22 +4,22 @@ from . import views
 
 urlpatterns = [
     url(
-        r'^api/v1/genes/(?P<pk>[0-9]+)$',
+        r'^api/genes/(?P<pk>[0-9]+)$',
         views.get_delete_update_gene,
         name='get_delete_update_gene'
     ),
     url(
-        r'^api/v1/genes/$',
+        r'^api/genes/$',
         views.get_post_genes,
         name='get_post_genes'
     ),
     url(
-        r'^api/v1/proteins/(?P<pk>[0-9]+)$',
+        r'^api/proteins/(?P<pk>[0-9]+)$',
         views.get_delete_update_protein,
         name='get_delete_update_protein'
     ),
     url(
-        r'^api/v1/proteins/$',
+        r'^api/proteins/$',
         views.get_post_proteins,
         name='get_post_proteins'
     )

e8756086eb97870a922c9e464eeb4b19ffa51b42
Patrick Oliver <poliver24@hotmail.com>
2020-11-29 01:05:59 +0000

    Test added for Get All Genes Route; Test Failing

--- immunocore_project/immunocore_app/tests/test_views.py
+++ immunocore_project/immunocore_app/tests/test_views.py
@@ -8,3 +8,25 @@ from ..serializers import GeneSerializer, ProteinSerializer
 
 # initialize the APIClient app
 client = Client()
+
+class GetAllGenesTest(TestCase):
+    """ Test module for GET all genes API """
+
+    def setUp(self):
+        Gene.objects.create(
+            name='TP53', sequence='CCAG')
+        Gene.objects.create(
+            name='TNF', sequence='CTGA')
+        Gene.objects.create(
+            name='EGFR', sequence='CCGG')
+        Gene.objects.create(
+            name='VEGFA', sequence='TTGA')
+
+    def test_get_all_genes(self):
+        # get API response
+        response = client.get(reverse('get_post_genes'))
+        # get data from db
+        genes = Gene.objects.all()
+        serializer = GeneSerializer(genes, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

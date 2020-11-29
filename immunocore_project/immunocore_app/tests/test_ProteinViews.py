4dd629dfbee56bc209f9a3ddc747338641d254e7
Patrick Oliver <poliver24@hotmail.com>
2020-11-29 02:02:12 +0000

    Tests added for single protein views; both tests failing

--- immunocore_project/immunocore_app/tests/test_ProteinViews.py
+++ immunocore_project/immunocore_app/tests/test_ProteinViews.py
@@ -35,3 +35,31 @@ class GetAllProteinsTest(TestCase):
         serializer = ProteinSerializer(proteins, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetSingleProteinTest(TestCase):
+    """ Test module for GET single Protein API """
+
+    def setUp(self):
+        Gene.objects.create(
+            name='TP53', sequence='CCAG')
+        Gene.objects.create(
+            name='TNF', sequence='CTGA')
+
+        gene_TP53 = Gene.objects.get(name='TP53')
+        gene_TNF = Gene.objects.get(name='TNF')
+
+        self.isoform_5 = Protein.objects.create(gene=gene_TP53, name='isoform 5', sequence='MDLVLK')
+        self.isoform_2 = Protein.objects.create(gene=gene_TNF, name='isoform 2', sequence='PGQEA')
+
+    def test_get_valid_single_protein(self):
+        response = client.get(
+            reverse('get_delete_update_protein', kwargs={'pk': self.isoform_2.pk}))
+        protein = Protein.objects.get(pk=self.isoform_2.pk)
+        serializer = ProteinSerializer(protein)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_protein(self):
+        response = client.get(
+            reverse('get_delete_update_protein', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

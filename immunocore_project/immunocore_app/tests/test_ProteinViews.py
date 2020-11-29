62c33e245c8fe6b5ea73ad972b31fefd72c2ccc0
Patrick Oliver <poliver24@hotmail.com>
2020-11-29 01:49:06 +0000

    Split test files

--- immunocore_project/immunocore_app/tests/test_ProteinViews.py
+++ immunocore_project/immunocore_app/tests/test_ProteinViews.py
@@ -2,34 +2,12 @@ import json
 from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import reverse
-from ..models import Gene, Protein
-from ..serializers import GeneSerializer, ProteinSerializer
+from ..models import Protein, Gene
+from ..serializers import ProteinSerializer
 
 
 # initialize the APIClient app
 client = Client()
-
-class GetAllGenesTest(TestCase):
-    """ Test module for GET all genes API """
-
-    def setUp(self):
-        Gene.objects.create(
-            name='TP53', sequence='CCAG')
-        Gene.objects.create(
-            name='TNF', sequence='CTGA')
-        Gene.objects.create(
-            name='EGFR', sequence='CCGG')
-        Gene.objects.create(
-            name='VEGFA', sequence='TTGA')
-
-    def test_get_all_genes(self):
-        # get API response
-        response = client.get(reverse('get_post_genes'))
-        # get data from db
-        genes = Gene.objects.all()
-        serializer = GeneSerializer(genes, many=True)
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class GetAllProteinsTest(TestCase):
     """ Test module for GET all genes API """

2daf8d56f9e3e61684adbee2039575e788a0cc53
Patrick Oliver <poliver24@hotmail.com>
2020-11-29 18:13:16 +0000

    Axios installed; Axios BaseURL set; Axios CRUD Services created

--- client/src/services/GeneService.js
+++ client/src/services/GeneService.js
@@ -1,1 +1,30 @@
 // Service for sending requests to Gene API routes
+import http from "../http-common";
+
+const getGenes = () => {
+    return http.get("/genes");
+};
+
+const getGene = () => {
+    return http.get(`/genes/${id}`);
+};
+
+const createGene = () => {
+    return http.post("/genes", data);
+};
+
+const updateGene = () => {
+    return http.put(`/genes/${id}`, data);
+};
+
+const deleteGene = () => {
+    return http.delete(`/genes/${id}`)
+}
+
+export default {
+    getGenes,
+    getGene,
+    createGene,
+    updateGene,
+    deleteGene
+};
--- client/src/services/ProteinService.js
+++ client/src/services/ProteinService.js
@@ -1,1 +1,30 @@
 // Service for sending requests to Protein API routes
+import http from "../http-common";
+
+const getProteins = () => {
+  return http.get("/Proteins");
+};
+
+const getProtein = () => {
+  return http.get(`/Proteins/${id}`);
+};
+
+const createProtein = () => {
+  return http.post("/Proteins", data);
+};
+
+const updateProtein = () => {
+  return http.put(`/Proteins/${id}`, data);
+};
+
+const deleteProtein = () => {
+  return http.delete(`/Proteins/${id}`);
+};
+
+export default {
+  getProteins,
+  getProtein,
+  createProtein,
+  updateProtein,
+  deleteProtein,
+};

7a856b4d0ebf60679a5f648d87089ade729f3bc7
Patrick Oliver <poliver24@hotmail.com>
2020-11-30 17:49:28 +0000

    Nested serializer updated to allow simple creation of new protein

--- server/immunocore_app/serializers.py
+++ server/immunocore_app/serializers.py
@@ -8,6 +8,19 @@ class GeneSerializer(serializers.ModelSerializer):
         fields = ('id','name', 'sequence')
 
 class ProteinSerializer(serializers.ModelSerializer):
+    related_gene = serializers.SerializerMethodField(read_only=True)
+    gene = serializers.CharField(write_only=True)
     class Meta:
         model = Protein
-        fields = ('id', 'name', 'sequence', 'gene')
+        fields = ('id', 'name', 'sequence', 'related_gene', 'gene')
+        # read_only_fields = ('gene')
+
+    def get_related_gene(self, obj):
+        return GeneSerializer(obj.related_gene).data
+
+    def create(self, validated_data):
+        print(validated_data)
+        gene_name = validated_data.pop('gene')
+        gene = Gene.objects.get(name=gene_name)
+        protein = Protein.objects.create(related_gene=gene, **validated_data)
+        return protein
--- server/immunocore_app/urls.py
+++ server/immunocore_app/urls.py
@@ -3,31 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('api/genes/<int:pk>/', views.get_delete_update_gene, name='get_delete_update_gene'),
-    path('api/genes/', views.get_post_genes, name='get_post_genes'),
-    path('api/proteins/<int:pk>/', views.get_delete_update_protein, name='get_delete_update_protein'),
-    path('api/proteins/', views.get_post_proteins, name='get_post_proteins')
+    path('api/genes/<int:pk>/', views.SingleGene.as_view(), name='get_delete_update_gene'),
+    path('api/genes/', views.ListGenes.as_view(), name='get_post_genes'),
+    path('api/proteins/<int:pk>/', views.SingleProtein.as_view(), name='get_delete_update_protein'),
+    path('api/proteins/', views.ListProteins.as_view(), name='get_post_proteins')
 ]
-
-# urlpatterns = [
-#     url(
-#         r'^api/genes/(?P<pk>[0-9]+)$',
-#         views.get_delete_update_gene,
-#         name='get_delete_update_gene'
-#     ),
-#     url(
-#         r'^api/genes/$',
-#         views.get_post_genes,
-#         name='get_post_genes'
-#     ),
-#     url(
-#         r'^api/proteins/(?P<pk>[0-9]+)$',
-#         views.get_delete_update_protein,
-#         name='get_delete_update_protein'
-#     ),
-#     url(
-#         r'^api/proteins/$',
-#         views.get_post_proteins,
-#         name='get_post_proteins'
-#     )
-# ]

64b77642e048f2f52b0fd7c527627245ebb53ab0
Patrick Oliver <poliver24@hotmail.com>
2020-12-02 02:08:14 +0000

    Updated Base_URL on frontend; Added Netlify site as allowed CORS origin for backend

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000/api',
-    headers: {
-        "Content-type": "application/json"
-    }
+  baseURL: "https://genestoredb.herokuapp.com/api",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
